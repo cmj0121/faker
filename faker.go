7653fc25bda184fa68c2b4d1a5cd14ff6b392155
cmj <cmj@cmj.tw>
2020-12-02 12:07:54 +0800

    [Faker] set the random byte to fixed length bytes
    	- support faker fixed-length bytes
    	- support faker string with max-length is FAKE_MAX_CAP=32

--- faker.go
+++ faker.go
@@ -6,11 +6,18 @@ import (
 	"sync"
 )
 
+const (
+	FAKE_MAX_CAP = 32
+)
+
 var (
 	fake_lock = sync.Mutex{}
 )
 
 func Fake(in interface{}) (err error) {
+	fake_lock.Lock()
+	defer fake_lock.Unlock()
+
 	value := reflect.ValueOf(in)
 
 	if value.Kind() != reflect.Ptr {
@@ -26,17 +33,43 @@ func Fake(in interface{}) (err error) {
 }
 
 func fake(value reflect.Value) (err error) {
-	fake_lock.Lock()
-	defer fake_lock.Unlock()
-
 	switch kind := value.Kind(); kind {
-	case reflect.Int, reflect.Int8:
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		value.SetInt(generator.Int63())
+	case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		value.SetUint(uint64(generator.Int63()))
 	case reflect.Float64, reflect.Float32:
 		value.SetFloat(generator.Float64())
+	case reflect.String:
+		size := int(generator.Int63() % FAKE_MAX_CAP)
+		data := fakeBytes(size, nil)
+		value.SetString(string(data))
+	case reflect.Slice, reflect.Array:
+		for idx := 0; idx < value.Len(); idx++ {
+			if err = fake(value.Index(idx)); err != nil {
+				err = fmt.Errorf("cannot set #%d on %v: %v", idx, value.Type(), err)
+				return
+			}
+		}
+		return
 	default:
 		err = fmt.Errorf("cannot set fake for reflect.Kind: %v", kind)
 		return
+	}
+
+	return
+}
+
+func fakeBytes(size int, pool []byte) (out []byte) {
+	switch l := int64(len(pool)); l {
+	case 0:
+		for idx := 0; idx < size; idx++ {
+			out = append(out, byte(generator.Int63()))
+		}
+	default:
+		for idx := 0; idx < size; idx++ {
+			out = append(out, pool[generator.Int63()%l])
+		}
 	}
 
 	return
--- faker_test.go
+++ faker_test.go
@@ -2,51 +2,12 @@ package faker
 
 import (
 	"math/rand"
+	"reflect"
 	"testing"
 )
 
 func TestFakerInt(t *testing.T) {
-	x := 10
-
-	Seed(0)
-	if err := Fake(&x); err != nil {
-		t.Fatalf("fake %T: %v", x, err)
-	} else if x != 8717895732742165505 {
-		t.Fatalf("fake int: %v <> 8717895732742165505", x)
-	}
-
-	y := int8(10)
-
-	Seed(0)
-	if err := Fake(&y); err != nil {
-		t.Fatalf("fake %T: %v", y, err)
-	} else if y != 1 {
-		t.Fatalf("fake int: %v <> 1", y)
-	}
-}
-
-func TestFakerFloat(t *testing.T) {
-	x := 1.2
-
-	Seed(0)
-	if err := Fake(&x); err != nil {
-		t.Fatalf("fake %T: %v", x, err)
-	} else if x != 0.9451961492941164 {
-		t.Fatalf("fake int: %v <> 0.9451961492941164", x)
-	}
-
-	y := float32(1.2)
-
-	Seed(0)
-	if err := Fake(&y); err != nil {
-		t.Fatalf("fake %T: %v", y, err)
-	} else if y != 0.9451961492941164 {
-		t.Fatalf("fake int: %v <> 0.9451961492941164", y)
-	}
-}
-
-func TestFakerIntRepeat(t *testing.T) {
-	x := 10
+	var x int
 	x_answers := []int{
 		8717895732742165505,
 		2259404117704393152,
@@ -64,7 +25,7 @@ func TestFakerIntRepeat(t *testing.T) {
 		}
 	}
 
-	y := int8(10)
+	var y int8
 	y_answers := []int8{
 		-81,
 		57,
@@ -78,6 +39,101 @@ func TestFakerIntRepeat(t *testing.T) {
 			t.Fatalf("cannot set faker to %T: %v", y, err)
 		} else if y != ans {
 			t.Fatalf("fake %v <> %v", y, ans)
+		}
+	}
+}
+
+func TestFakerFloat(t *testing.T) {
+	var x float64
+	x_answers := []float64{
+		0.9451961492941164,
+		0.24496508529377975,
+		0.6559562651954052,
+		0.05434383959970039,
+		0.36758720663245853,
+	}
+
+	Seed(0)
+	for _, ans := range x_answers {
+		if err := Fake(&x); err != nil {
+			t.Fatalf("cannot set faker to %T: %v", x, err)
+		} else if x != ans {
+			t.Fatalf("fake %v <> %v", x, ans)
+		}
+	}
+
+	var y float32
+	y_answers := []float32{
+		0.28948045,
+		0.1924386,
+		0.65533215,
+		0.8971697,
+	}
+
+	rand.Seed(0)
+	for _, ans := range y_answers {
+		if err := Fake(&y); err != nil {
+			t.Fatalf("cannot set faker to %T: %v", y, err)
+		} else if y != ans {
+			t.Fatalf("fake %v <> %v", y, ans)
+		}
+	}
+}
+
+func TestFakerString(t *testing.T) {
+	var x string
+	x_answers := []string{
+		"\xc0",
+		"bJ\xaf9xQN\xf8D;\xb2\xa8Y\xc7_\xc3\xccj\xf2",
+		"Z\xaa \x92o\x04k\xaaf쑥\xa2",
+		"C#\xc2\xda@Z\xfe\xd6\x1e\xc1\\\xf7\x93\xed\x10:W\xc0,\x86\x9a\xf0i\xb7\xb5",
+		"7\xa2Rz0\xb2w\xe5",
+	}
+
+	Seed(0)
+	for _, ans := range x_answers {
+		if err := Fake(&x); err != nil {
+			t.Fatalf("cannot set faker to %T: %v", x, err)
+		} else if x != ans {
+			t.Fatalf("fake %#v <> %#v", x, ans)
+		}
+	}
+}
+
+func TestFakerSlice(t *testing.T) {
+	var x [4]byte
+	x_answers := [][4]byte{
+		{0x01, 0xc0, 0x73, 0x62},
+		{0x4a, 0xaf, 0x39, 0x78},
+		{0x51, 0x4e, 0xf8, 0x44},
+		{0x3b, 0xb2, 0xa8, 0x59},
+		{0xc7, 0x5f, 0xc3, 0xcc},
+	}
+
+	Seed(0)
+	for _, ans := range x_answers {
+		if err := Fake(&x); err != nil {
+			t.Fatalf("cannot set faker to %T: %v", x, err)
+		} else if !reflect.DeepEqual(x, ans) {
+			t.Fatalf("fake %#v <> %#v", x, ans)
+		}
+	}
+
+	var y [7]byte
+	y_answers := [][7]byte{
+		{0x01, 0xc0, 0x73, 0x62, 0x4a, 0xaf, 0x39},
+		{0x78, 0x51, 0x4e, 0xf8, 0x44, 0x3b, 0xb2},
+		{0xa8, 0x59, 0xc7, 0x5f, 0xc3, 0xcc, 0x6a},
+		{0xf2, 0x6d, 0x5a, 0xaa, 0x20, 0x92, 0x6f},
+		{0x04, 0x6b, 0xaa, 0x66, 0xec, 0x91, 0xa5},
+	}
+
+	Seed(0)
+	for _, ans := range y_answers {
+		if err := Fake(&y); err != nil {
+			t.Fatalf("cannot set faker to %T: %v", y, err)
+		} else if !reflect.DeepEqual(y, ans) {
+			t.Fatalf("fake %#v <> %#v", y, ans)
 		}
 	}
 }
@@ -96,6 +152,39 @@ func BenchmarkFakeInt(b *testing.B) {
 func BenchmarkFakeFloat(b *testing.B) {
 	b.RunParallel(func(pb *testing.PB) {
 		var x float64
+
+		for pb.Next() {
+			// generate the fake int
+			Fake(&x)
+		}
+	})
+}
+
+func BenchmarkFakeString(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		var x string
+
+		for pb.Next() {
+			// generate the fake int
+			Fake(&x)
+		}
+	})
+}
+
+func BenchmarkFakeSlice32(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		var x [32]byte
+
+		for pb.Next() {
+			// generate the fake int
+			Fake(&x)
+		}
+	})
+}
+
+func BenchmarkFakeSlice64(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		var x [64]byte
 
 		for pb.Next() {
 			// generate the fake int
